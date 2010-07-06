bfdb5fb07a9703b0552ff3fbe6b0913ebee2cf5f
n00p <admin@geekycode.net>
2010-07-07 03:22:17 +0930

    ... should have been part of the previous commit. sorry :(

---
+++ src/EventListNode.c
@@ -0,0 +1,33 @@
+#ifndef __EVENTLISTNODE_C
+#define __EVENTLISTNODE_C
+
+// Copyright Sebastian Ramadan 2009
+/** \file EventListNode.c
+ *  \brief Function prototype definition (Event), and linked list structure (EventListNode) for defining/storing Event handler functions. Function for creating/initializing EventListNode instances.
+ */
+
+typedef int (* Event)(struct SockListNode *Node, unsigned char *Parsed, unsigned char *Unparsed, unsigned char *End);
+
+struct EventListNode {
+	struct EventListNode *Next;
+	Event Function;
+};
+
+struct EventListNode *EventList_CreateNode(struct EventListNode *Next, Event Function) {
+	struct EventListNode *Node;
+
+	if ((Node = malloc(sizeof(*Node))) == NULL) { return NULL; }
+	Node->Next = Next;
+	Node->Function = Function;
+
+	return Node;
+}
+
+void EventListNode_Raise(struct SockListNode *SockNode, struct EventListNode *EventNode, unsigned char *Parsed, unsigned char *Unparsed, unsigned char *End) {
+	if (EventNode == NULL) { return; }
+	if (EventNode->Function == NULL) { return; }
+	EventNode->Function(SockNode, Parsed, Unparsed, End);
+	EventListNode_Raise(SockNode, EventNode->Next, Parsed, Unparsed, End);
+}
+
+#endif
