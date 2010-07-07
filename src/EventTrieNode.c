d5c6dae2b4e4be1b2ea8ab9f46ad3a9eaa49ff5c
n00p <admin@geekycode.net>
2010-07-07 20:59:13 +0930

    Fixed some memory leaks, tidied some code & comments.
    Removed unnecessary comments, tidied up if statements, refactored redundant code, fixed memory leaks when EventTrie_CreateNode/RelativeTrie_CreateNode would fail and added some useful comments.

--- src/EventTrieNode.c
+++ src/EventTrieNode.c
@@ -104,7 +104,7 @@ unsigned int EventTrie_AddNode(struct TrieNode *Parent, unsigned char *Prefix, u
             e = EventTrie_CreateNode(NULL, Result->Parent);
             if (e == NULL) { return 0; }
 
-			Trie_InitNode((struct TrieNode *) e, Result->Node.Group, Result->Node.GroupCount, Result->Node.Prefix, Result->Node.PrefixLength, (Destroy) EventTrie_DestroyNode);
+            Trie_InitNode((struct TrieNode *) e, Result->Node.Group, Result->Node.GroupCount, Result->Node.Prefix, Result->Node.PrefixLength, (Destroy) EventTrie_DestroyNode);
             parent->Node.Group[Charmap[Prefix[PrefixIndex - Result->Node.PrefixLength]]] = (struct TrieNode *) e;
             free(Result);
         }
@@ -117,7 +117,7 @@ unsigned int EventTrie_AddNode(struct TrieNode *Parent, unsigned char *Prefix, u
         struct TrieNode *n;
         
         if (NodeIndex > 0 && NodeIndex < Result->Node.PrefixLength) {
-			struct TrieNode **g;
+            struct TrieNode **g;
 
             /* situation ii.
              * The node doesn't exist, and it's path lies between 2 nodes that share a branch for example... in the middle of a branch
@@ -131,82 +131,82 @@ unsigned int EventTrie_AddNode(struct TrieNode *Parent, unsigned char *Prefix, u
             memcpy(p, Result->Node.Prefix, NodeIndex);
             p[NodeIndex] = '\0';
             
-			g = Trie_CreateGroup();
-			if (g == NULL) {
-				free(p);
-				return 0;
-			}
+            g = Trie_CreateGroup();
+            if (g == NULL) {
+                free(p);
+                return 0;
+            }
 
-			/* The following branch executes when adding a node causes a branch fork
-			 *    eg. the collection contains "foobar" and we're adding "foobah". The code below will add a "fooba" item to the root node,
-			 *    so we need to add a "h" item to the "fooba" item's nodes.
-			 */
+            /* The following branch executes when adding a node causes a branch fork
+             *    eg. the collection contains "foobar" and we're adding "foobah". The code below will add a "fooba" item to the root node,
+             *    so we need to add a "h" item to the "fooba" item's nodes.
+             */
             if (Charmap[Prefix[PrefixIndex]] != '\0') {
                 t = RelativeTrie_CreateNode(parent);
                 n = Trie_InitNode((struct TrieNode *) t, g, 1, p, NodeIndex, (Destroy) RelativeTrie_DestroyNode);
             }
             /* The following branch executes when adding a node with a prefix that already exists as part of another node's prefix.
-			 *    eg. the collection contains "foobar" and we're adding "foo".
-			 */
+             *    eg. the collection contains "foobar" and we're adding "foo".
+             */
             else {
                 e = EventTrie_CreateNode(NULL, parent);
                 t = (struct RelativeTrieNode *) e;
                 n = Trie_InitNode((struct TrieNode *) e, g, 1, p, NodeIndex, (Destroy) EventTrie_DestroyNode);
             }
 
-			/* n will be allocated in one of the above branches, so it'll also need to be checked :) */
-			if (n == NULL) {
-				free(g);
-				free(p);
+            /* n will be allocated in one of the above branches, so it'll also need to be checked :) */
+            if (n == NULL) {
+                free(g);
+                free(p);
                 return 0;
             }
             
-			/* The newly created node is to be inserted before "foobar" (eg. "foo"). The old node's prefix and prefix length needs updating. */
+            /* The newly created node is to be inserted before "foobar" (eg. "foo"). The old node's prefix and prefix length needs updating. */
             memmove(Result->Node.Prefix, Result->Node.Prefix + NodeIndex, Result->Node.PrefixLength -= NodeIndex);
             Result->Node.Prefix[Result->Node.PrefixLength] = '\0';
             
-			/* Insert the new into it's correct (hopefully) position */
+            /* Insert the new into it's correct (hopefully) position */
             (*parent)->Node.Group[*p] = n;
 
-			/* Add "foobar" as a node within the new node and update it's Parent value to reflect the change */
+            /* Add "foobar" as a node within the new node and update it's Parent value to reflect the change */
             (n->Group = g)[Result->Node.Prefix[0]] = (struct TrieNode *) Result;
             Result->Parent = t->This;
             NodeIndex = 0;
             Result = (struct RelativeTrieNode *) n;
         }
         
-		/* The following branch executes when:
-		 * i. adding a node causes a branch fork
-		 *    eg. the collection contains "foobar" and we're adding "foobah". The code above has added a "fooba" item to the root node,
-		 *    so we need to add a "h" item to the "fooba" item's nodes.
-		 * ii. adding a node is a simple case of just appending the node onto the end of an existing prefix (this actually also covers point i.)
-		 *    eg. the collection contains a "foo" item and we're adding "foobar" (eg. adding "bar" to the "foo" item's nodes)
-		 *           ---------
-		 * ... It will *not* execute when adding a node with a prefix that already exists as part of another node's prefix.
-	     *    eg. the collection contains "foobar" and we're adding "foo".
-		 */
+        /* The following branch executes when:
+         * i. adding a node causes a branch fork
+         *    eg. the collection contains "foobar" and we're adding "foobah". The code above has added a "fooba" item to the root node,
+         *    so we need to add a "h" item to the "fooba" item's nodes.
+         * ii. adding a node is a simple case of just appending the node onto the end of an existing prefix (this actually also covers point i.)
+         *    eg. the collection contains a "foo" item and we're adding "foobar" (eg. adding "bar" to the "foo" item's nodes)
+         *           ---------
+         * ... It will *not* execute when adding a node with a prefix that already exists as part of another node's prefix.
+         *    eg. the collection contains "foobar" and we're adding "foo".
+         */
         if (Charmap[Prefix[PrefixIndex]] != '\0') {
             unsigned int len;
             unsigned char *o;
 
-			/* Calculate the length of the new nodes prefix. */
+            /* Calculate the length of the new nodes prefix. */
             for (q = Prefix + PrefixIndex; Charmap[*q]; q++);
             len = q - Prefix;
             
-			/* Allocate and set the new nodes prefix. */
+            /* Allocate and set the new nodes prefix. */
             p = malloc(len + 1);
             if (p == NULL) { return 0; }
             for (o = p, q = Prefix + PrefixIndex; *o = Charmap[*q]; o++, q++);
             
-			/* Create the node that will store the event handler */
+            /* Create the node that will store the event handler */
             e = EventTrie_CreateNode(NULL, Result->Parent);
             if (e == NULL) {
                 free(p);
                 return 0;
             }
 
-			/* Initialize the node that will store the event handler */
-			Trie_InitNode((struct TrieNode *) e, Trie_CreateGroup(), 0, p, len, (Destroy) EventTrie_DestroyNode);
+            /* Initialize the node that will store the event handler */
+            Trie_InitNode((struct TrieNode *) e, Trie_CreateGroup(), 0, p, len, (Destroy) EventTrie_DestroyNode);
 
             Result->Node.Group[*p] = (struct TrieNode *) e;
             e->Node.Parent = Result->This;
