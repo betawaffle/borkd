61d60aae8a1a3fd7915b69c3ead7c7750ba0a95a
Andrew Hodges <betawaffle@gmail.com>
2010-07-07 17:43:15 -0400

    Added SockEvent

---
+++ src/SockEvent.c
@@ -0,0 +1,20 @@
+/* Copyright (c) 2010, Sebastian Ramadan and Andrew Hodges. All rights reserved. */
+
+#include "SockEvent.h"
+
+SockEvent SockEvent_Create(SockListNode SockNode, unsigned char *Parsed, unsigned char *Unparsed, unsigned char *End) {
+    SockEvent Event; Event = malloc(sizeof(*Event));
+    
+    if (Event == NULL) { return NULL; }
+    
+    Event->SockNode = SockNode;
+    Event->Parsed = Parsed;
+    Event->Unparsed = Unparsed;
+    Event->End = End;
+    
+    return Event;
+}
+
+void SockEvent_Destroy(void *Event) {
+    free(Event);
+}
